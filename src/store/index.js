7830c48f5214973149feb99537d01882ade8f1d4
dsoto1-rbi <dsoto1@rbi.com>
2022-06-29 18:56:52 -0500

    [feature] - install redux thunk and add firebase url

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import categoryReducer from "./reducers/category.reducer";
 import productReducer from "./reducers/products.reducer";
+import thunk from "redux-thunk";
 
 const RootReducer = combineReducers({
   category: categoryReducer,
   product: productReducer,
 });
 
-export default createStore(RootReducer);
+export default createStore(RootReducer, applyMiddleware(thunk));
